00b9a0da58fc5326b1b2e98884309ced04fff8a1
Varrenlad Irredail <varrenlad@home.local>
2017-10-02 02:54:18 +0700

    Release build. Used technologies: Bootstrap v4 alpha 6 Showdown Markdown parser: https://github.com/showdownjs/showdown To markdown HTML parser: https://github.com/domchristie/to-markdown FontAwesome: http://fontawesome.io/ Fira Mono woff2: https://github.com/mozilla/Fira/tree/master/woff2 Open Sans woff2: https://github.com/FontFaceKit/open-sans Some tutorials from W3Schools atumanov CSS and JQuery scripts Material Floating button: https://codepad.co/snippet/0PNF4OPp The Almighty StackOverflow

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,18 @@
+var html;
+
+function t_export(type) {
+  switch (type) {
+    case 0:
+      document.getElementById("t-export").value = html;
+      break;
+    case 1:
+      document.getElementById("t-export").value = $('#parced-data').html();
+      break;
+    default:
+      break;
+  }
+}
+
 function doSearch(engine) {
   query = document.getElementById("searchText").value;
   switch (engine) {
@@ -22,16 +37,75 @@ function from_scratch() {
 
 function parser() {
   var converter = new showdown.Converter();
+  converter.setOption('simpleLineBreaks', true);
+  converter.setOption('noHeaderId', true);
   let text      = document.getElementById("textbox").value;
-  var html      = converter.makeHtml(text);
-  return html;
+  html          = converter.makeHtml(text);
 }
 
-function apply_styles(html_to_push) {
-  $('#parced-data').append(html_to_push);
+function apply_styles() {
+  $('#parced-data').append(html);
+
+    //Automatic style by atumanov
+  $.extend($.expr[":"], { "startsWith": function(elem, i, match, array) { return (elem.textContent || elem.innerText || "").toLowerCase ().indexOf((match[3] || "").toLowerCase()) == 0; } });
+  
+    $("p:startsWith('# ')").addClass('bash');
+    $("p:contains('C:\\>')").addClass('winshell');
+    $("p:startsWith('PS ')").addClass('powershell');
+    $("p:contains('PLESK_ERROR:')").addClass('pleskerr');
+    $("p:contains('PLESK_WARN:')").addClass('pleskwarn');
+    $("p:contains('PLESK_INFO:')").addClass('pleskinfo');
+    $("p:contains('MYSQL_LIN:')").addClass('bash');
+    $("p:contains('MYSQL_WIN:')").addClass('winshell');
+    $("p:startsWith('Note: ')").addClass('note');
+    $("p:contains('CONFIG_TEXT:')").addClass('configtext');
+    $("p:startsWith('Warning:')").addClass('warning');
+  
+//Automatic cut of words and symbols
+    $("p:contains('C:\\> ')")
+        .each(function () {
+            $(this).html($(this).html().replace(/C:\\&gt; /g, ''));
+    })
+    
+//    $("p:startsWith('PS ')")
+//        .each(function () {
+//            $(this).html($(this).html().replace(/PS /g, ''));
+//    })
+    
+    $("p:contains('PLESK_ERROR: ')")
+        .each(function () {
+            $(this).html($(this).html().replace(/PLESK_ERROR: /g, ''));
+        })
+
+    $("p:contains('PLESK_INFO: ')")
+        .each(function () {
+            $(this).html($(this).html().replace(/PLESK_INFO: /g, ''));
+        })
+    $("p:contains('PLESK_WARN: ')")
+        .each(function () {
+            $(this).html($(this).html().replace(/PLESK_WARN: /g, ''));
+        })
+    $("p:contains('MYSQL_LIN: ')")
+        .each(function () {
+            $(this).html($(this).html().replace(/MYSQL_LIN: /g, ''));
+        })
+
+    $("p:contains('MYSQL_WIN: ')")
+        .each(function () {
+            $(this).html($(this).html().replace(/MYSQL_WIN: /g, ''));
+        })
+    $("p:contains('CONFIG_TEXT: ')")
+        .each(function () {
+            $(this).html($(this).html().replace(/CONFIG_TEXT: /g, ''));
+        })
 }
 
 function mdparse() {
   from_scratch();
   apply_styles(parser());
+}
+
+function turndown() {
+  document.getElementById("textbox").value = toMarkdown(document.getElementById("t-import").value);
+  mdparse();
 }
